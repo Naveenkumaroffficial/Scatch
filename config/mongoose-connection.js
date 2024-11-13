f76bb3edce4363b8baeb9696e4a1a06217b0276c
Naveen Kumar <git add README.md>
2024-11-13 22:52:41 +0530

    testing

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const debug=require("debug")("development:mongoose")
 
 mongoose.connect("mongodb://127.0.0.1:27017/scatch")
 .then(()=>{
-    console.log("connected");
+    debug("connected");
     
 })
 .catch((err)=>{
-    console.log(err);
+    debug(err);
+
+    
     
 
 })
